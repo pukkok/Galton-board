196923405657e98802385ae60990da24e57c0519
Pukkok <krystalgggg@gmail.com>
2024-12-14 23:52:09 +0900

    ball_btns 배열을 리턴하는 방식으로 변경경

--- src/buttons/ball_btns.js
+++ src/buttons/ball_btns.js
@@ -1,12 +1,13 @@
 import ballCounts from "../../ballCounts.js"
 
-const ballBtns = (parent) => {
+const ballBtns = () => {
   const btns = ballCounts.map(ball => {
     const button = document.createElement('button')
     button.innerHTML = ball + 'balls'
     return button
   })
-  parent.append(...btns)
+  
+  return btns
 }
 
 export default ballBtns
