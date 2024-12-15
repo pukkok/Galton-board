6401c126364b0bdc20c62c29086ba989caf7c8ac
Pukkok <krystalgggg@gmail.com>
2024-12-15 22:55:06 +0900

    상수 주석 추가

--- src/canvas/constants.js
+++ src/canvas/constants.js
@@ -1,4 +1,4 @@
-export const rows = 8
-export const pinRadius = 5
-export const pinVerticalGap = 70
-export const topY = 50
+export const rows = 8 // * 최대 행의 개수 (갈림길을 만드는 동그라미의 마지막 행)
+export const pinRadius = 5 // * 공의 반지름
+export const pinVerticalGap = 70 // * 공의 세로 간격
+export const topY = 30 // * 핀이 시작되는 상단 마진
