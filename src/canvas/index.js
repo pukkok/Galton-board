1d8f5c28eaef8fc54f3ee0efa6bb098bd329dca5
Pukkok <krystalgggg@gmail.com>
2024-12-15 20:23:22 +0900

    index로 canvas 모든 파일 관리

---
+++ src/canvas/index.js
@@ -0,0 +1,6 @@
+import { startAnimation } from './animation.js'
+import { startEvent, pauseEvent, resetEvent } from './events.js'
+
+export { startEvent, pauseEvent, resetEvent }
+
+startAnimation()
