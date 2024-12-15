2edbba27b119d715ef56f97d7cca49cfd91da2a7
Pukkok <krystalgggg@gmail.com>
2024-12-15 19:04:22 +0900

    버튼 전체 연결 완료

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ballSelectOption from "./controls/ball_select_option.js";
+import startBtn from "./controls/start_btn.js"
+import pauseBtn from "./controls/pause_btn.js"
+import resetBtn from "./controls/reset_btn.js"
+
 const box = document.querySelector('.controls')
 
 const selectOption = ballSelectOption()
-box.append(selectOption)
+const start = startBtn()
+const pause = pauseBtn()
+const reset = resetBtn()
+box.append(selectOption, start, pause, reset)
